9b5f75edc4e5f6024f86034a524b76929f112ca9
miekki-jerry <hello@bogumilluc.pl>
2025-09-23 11:11:07 +0200

    chore(git): ignore .env, dist, node_modules and stop tracking

--- dist/routes/voice-routes.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Voice Service API Routes
- * Defines all REST endpoints for the voice service
- */
-declare const router: import("express-serve-static-core").Router;
-export { router as voiceRoutes };
-//# sourceMappingURL=voice-routes.d.ts.map
